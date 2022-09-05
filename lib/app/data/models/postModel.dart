f01f5bdf71bbbb77cf84add0b3d9f2935260d97c
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-05 19:03:41 +0600

    updated

---
+++ lib/app/data/models/postModel.dart
@@ -0,0 +1,37 @@
+// To parse this JSON data, do
+//
+//     final postModel = postModelFromJson(jsonString);
+
+import 'dart:convert';
+
+List<PostModel> postModelFromJson(String str) => List<PostModel>.from(json.decode(str).map((x) => PostModel.fromJson(x)));
+
+String postModelToJson(List<PostModel> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class PostModel {
+    PostModel({
+        this.userId,
+        this.id,
+        this.title,
+        this.body,
+    });
+
+    final int? id;
+    final int? userId;
+    final String? title;
+    final String? body;
+
+    factory PostModel.fromJson(Map<String, dynamic> json) => PostModel(
+        userId: json["userId"] == null ? null : json["userId"],
+        id: json["id"] == null ? null : json["id"],
+        title: json["title"] == null ? null : json["title"],
+        body: json["body"] == null ? null : json["body"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "userId": userId == null ? null : userId,
+        "id": id == null ? null : id,
+        "title": title == null ? null : title,
+        "body": body == null ? null : body,
+    };
+}
--- lib/app/data/providers/user_provider.dart
+++ lib/app/data/providers/user_provider.dart
@@ -1,8 +1,31 @@
 import 'package:get/get.dart';
+import 'package:getx/app/data/core/utils/App_urls.dart';
+import 'package:getx/app/data/models/postModel.dart';
 
 class UserProvider extends GetConnect {
   @override
   void onInit() {
     httpClient.baseUrl = 'YOUR-API-URL';
   }
+
+   Future<List<PostModel>?> getPosts(int page) async {
+    String url = AppConfig.getposts;
+    print('$url');
+
+    try {
+      var response = await get(url);
+      if (response.statusCode == 200) {
+        List<PostModel> model = response.body['Search'];
+        return postfrom
+      } else {
+        return [];
+      }
+    } catch (error) {
+      print('movies data not found $error');
+      return [];
+    }
+  }
+  
+
+
 }
