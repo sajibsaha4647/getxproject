c6f7dbffe3bd806c7b9a3c182bfc4bcc6dd14cd6
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-05 16:38:40 +0600

    updated

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,9 +1,5 @@
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
-
-  final count = 0.obs;
-
-  void increment() => count.value++;
+  final count = 0;
 }
